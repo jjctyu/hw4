5e5b036cd59dae2bbab4fb271039042a1cca7684
citrusUprising <62976444+citrusUprising@users.noreply.github.com>
2023-12-06 15:26:47 -0800

    Added code to facilitate scroll

--- Assets/Our Assets/Scripting/World.cs
+++ Assets/Our Assets/Scripting/World.cs
@@ -8,16 +8,47 @@ public class World : MonoBehaviour
 {
 
     int scene = 0;
+    float distance = 0;
+    float speed = 8;
+    int sceneLength = 1920; //Change to 1920 - Mech width in pixels
+    bool mechWalking = false;
+    public GameObject background;
+    public GameObject mech;
     
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (mechWalking){
+            moveObjects(Time.deltaTime);
+        }
+        if(distance >= sceneLength){ //this code will assume scene is tied to locations and not unity scenes
+            /*if(scene < 2){
+                scene++;
+                //set distance to 0
+                //change background to scene
+                moveObjects (0);
+            } else {
+                //end event
+            }
+            */
+        }
+    }
+
+    void moveObjects (float movement){
+
+        distance+=speed*movement;
+
+        /*var mechTrans = mech.GetComponent<Transform>();
+        mechTrans.position.x = distance;
+        mech.transform = mechTrans;
+
+        var backgroundTrans = background.GetComponent<Transform>();
+        backgroundTrans.position.x = -distance*(1920/sceneLength);
+        background.transform = backgroundTrans;*/
     }
 }
