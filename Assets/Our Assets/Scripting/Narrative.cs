c9883da5e9dfd0c988a296c0154d6b3664ea9367
VerizonCao <caozongheng123@gmail.com>
2023-12-06 23:15:26 -0800

    add event system done and scene art

--- Assets/Our Assets/Scripting/Narrative.cs
+++ Assets/Our Assets/Scripting/Narrative.cs
@@ -5,32 +5,70 @@ using UnityEngine;
 // A Hacky file to store all the dialogues / text of the game. 
 
 
-public class Dialogue
+public class Event
 {
-    public string text;
+    public List<string> text;
     public List<string> options;
+    // some stats result in the follow.
+
 }
 
 
 public class Narrative : MonoBehaviour
 {
     
-    private List<Dialogue> events;
+    private List<Event> events;
+
+    public int loadSize()
+    {
+        return events.Count;
+    }
+
+    public Event getEventAtIndex(int index)
+    {
+        return events[index];
+    }
 
     private void Awake()
     {
-        events = new List<Dialogue>();
+        events = new List<Event>();
 
 
         //adding all the events
 
         //1 
-        Dialogue temp = new Dialogue();
-        temp.text = "The old machine(Player) wakes up in a small inhabited area; It is a large mobile factory and can see in the UI . With the Text displayed above, the factory starts heading towards the Northern Plains Biome. The player has a scrap, health, and energy stats. Immediately they are running into problems that put a drain on those resources.";
+        Event temp = new Event();
+        temp.text = new List<string>();
+        temp.text.Add("");
+        temp.text[0] += "Performing System Diagnostics¡­ \r\n Solarium Maximum Charge Capacity¡­ 37%\r\nIntake Charge Efficiency¡­ 6.57%\r\nTime to Full Charge ¡­ CANNOT BE DETERMINED\r\nAnalyzing procedures¡­\r\n¡­\r\nInitialize Odyssey Protocol\r\nDestination set: HOME\r\nPriority Alpha\r\n";
+        temp.text[0] += "The old machine(Player) wakes up in a small inhabited area; " +
+            "It is a large mobile factory and can see in the UI . With the Text displayed above, the factory starts heading towards the Northern Plains Biome. " +
+            "The player has a scrap, health, and energy stats. Immediately they are running into problems that put a drain on those resources.";
+
+        temp.text.Add("");
+        temp.text[1] += "Obstacle encountered\r\nAnalyzing¡­\r\nPath blocked by Oryza glaberrima¡­\r\nAnalyzing¡­\r\nRice Crop¡­ tended to by local Humans.";
+
+
+
+
         temp.options = new List<string>();
         temp.options.Add("-Option A| Proceed | potential damage to human crops");
         temp.options.Add("-Option B| Reorient | increases Travel Distance & Energy Usage");
         events.Add(temp);
+
+        //2
+        temp = new Event();
+        temp.text = new List<string>();
+        temp.text.Add("");
+        temp.text[0] += "The factory, after making several navigational decisions, eventually leaves the human settlement and enters an old world city. The buildings that remain standing are decrepit, and the area is littered with old barricades, mines, and cannons. There are bits of plant growth at the edges, but the landscape is predominantly rust and crumbled concrete.";
+        temp.text[0] += "Solarite charging opportunity discovered¡­\r\nAnalyzing environment compatibility¡­\r\n¡­\r\nWARNING: Shrapnel storm imminent\r\nSeeking shelter is advised\r\n";
+
+
+        temp.options = new List<string>();
+        temp.options.Add("-Option A | Charge | Enter low power mode to increase Energy, WARNING Hull will suffer structural damage from incoming storm");
+        temp.options.Add("-Option B | Shelter | Seek Shelter, WARNING Battery levels approaching  critical");
+        events.Add(temp);
+
     }
 
     void Start()
@@ -39,15 +77,7 @@ public class Narrative : MonoBehaviour
 
     }
 
-    public int loadSize()
-    {
-        return events.Count;
-    }
-
-    public Dialogue getEventAtIndex(int index)
-    {
-        return events[index];
-    }
+    
 
 
 }
--- Assets/Our Assets/Scripting/UserInput.cs
+++ Assets/Our Assets/Scripting/UserInput.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -16,6 +17,18 @@ public class UserInput : MonoBehaviour
     [SerializeField]
     private GameObject option2;
 
+    private Event currentEvent;
+    private int index = 0;
+    private int eventNumNow = 0;
+
+    [SerializeField]
+    private GameObject nextBotton;
+    [SerializeField]
+    private GameObject fowardButton;
+    [SerializeField]
+    private GameObject backwordButton;
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -28,13 +41,24 @@ public class UserInput : MonoBehaviour
     void Update()
     {
 
-
+        // click will force to next params if the text is very long.
+        // right click can go back. 
         if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("Pressed left-click.");
-            progress();
+            //Debug.Log("Pressed left-click.");
+
+            //forward();
         }
-            
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            //Debug.Log("Pressed right-click.");
+            //backward();
+        }
+
+
+        // event can only be progressed by click button or choose one of the dialogue.
+
 
 
     }
@@ -42,15 +66,35 @@ public class UserInput : MonoBehaviour
     // Render the next UI page
     private void progress()
     {
-        Dialogue d = scheduler.getNextEvent();
-        // render this dialogue on the canvas
+        Event d = scheduler.getNextEvent();
+        // render this events on the canvas
         if (d != null)
         {
-            Debug.Log("here in progress");
-            TMPro.TextMeshProUGUI tmpContext = context.GetComponent<TMPro.TextMeshProUGUI>();
-            tmpContext.SetText(d.text);
-            option1.GetComponentInChildren<TMPro.TextMeshProUGUI>().SetText(d.options[0]);
-            option2.GetComponentInChildren<TMPro.TextMeshProUGUI>().SetText(d.options[1]);
+            currentEvent = d;
+            index = 0;
+            renderContext();
+
+            //check the text count
+            if (currentEvent.text.Count > 1)
+            {
+                //show forward and backwordf botton
+                fowardButton.SetActive(true);
+                backwordButton.SetActive(true);
+                option1.SetActive(false);
+                option2.SetActive(false);
+            }
+            else
+            {
+                fowardButton.SetActive(false);
+                backwordButton.SetActive(false);
+                //show the options
+                option1.GetComponentInChildren<TMPro.TextMeshProUGUI>().SetText(d.options[0]);
+                option2.GetComponentInChildren<TMPro.TextMeshProUGUI>().SetText(d.options[1]);
+                option1.SetActive(true);
+                option2.SetActive(true);
+            }
+
+            
         }
         else
         {
@@ -60,11 +104,93 @@ public class UserInput : MonoBehaviour
 
     }
 
+
+    // go to the next page of the current event if has
+    private void forward()
+    {
+        if (currentEvent == null)
+        {
+            return;
+        }
+        if (index < currentEvent.text.Count - 1)
+        {
+            index++;
+            renderContext();
+        }
+    }
+
+    //back to the previous page of the current event if has
+    private void backward()
+    {
+        if (currentEvent == null)
+        {
+            return;
+        }
+        if (index > 0)
+        {
+            index--;
+            renderContext();
+        }
+    }
+
     public void ClickOption1()
     {
-        // getting the feedback info and following state change.
-
-        // test to print 1 now
         Debug.Log("option 1");
+        // now it's progress to next event
+        option1.SetActive(false);
+        option2.SetActive(false);
+        progress();
+        
+    }
+
+    public void ClickOption2()
+    {
+        Debug.Log("option 2");
+        // now it's progress to next event
+        progress();
+        option1.SetActive(false);
+        option2.SetActive(false);
+    }
+
+    public void ClickForward()
+    {
+        Debug.Log("forward");
+        forward();
+    }
+
+    public void ClickBackward()
+    {
+        Debug.Log("backward");
+        backward();
+    }
+
+    public void ClickNext()
+    {
+        Debug.Log("next");
+        //get next event or next wording(not in design yet)
+        progress();
+        //hide itself
+        nextBotton.SetActive(false);
+    }
+
+    private void renderContext()
+    {
+        TMPro.TextMeshProUGUI tmpContext = context.GetComponent<TMPro.TextMeshProUGUI>();
+        tmpContext.SetText(currentEvent.text[index]);
+
+        //if it's at the last page of this event, show options
+        if (currentEvent != null && index == currentEvent.text.Count - 1)
+        {
+            option1.SetActive(true);
+            option2.SetActive(true);
+            //show the options
+            option1.GetComponentInChildren<TMPro.TextMeshProUGUI>().SetText(currentEvent.options[0]);
+            option2.GetComponentInChildren<TMPro.TextMeshProUGUI>().SetText(currentEvent.options[1]);
+        }
+        else
+        {
+            option1.SetActive(false);
+            option2.SetActive(false);
+        }
     }
 }
